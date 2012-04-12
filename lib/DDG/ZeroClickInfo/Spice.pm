3df98de33a17d243930c2700598033b3d1fd5ac5
Torsten Raudssus <github@raudssus.de>
2012-04-12 23:30:58 +0200

    More attributes on zci spice

--- lib/DDG/ZeroClickInfo/Spice.pm
+++ lib/DDG/ZeroClickInfo/Spice.pm
@@ -4,8 +4,29 @@ use Moo;
 
 has call => (
 	is => 'ro',
-	required => 1,
 );
+
+has call_path => (
+	is => 'ro',
+	lazy_build => 1,
+	builder => '_build_call_path',
+);
+
+sub _build_call_path {
+	my ( $self ) = @_;
+	my @parts = split('::',$self->caller);
+	shift @parts;
+	my $path = join('/',map { s/([a-z])([A-Z])/$1_$2/; lc; } @parts);
+	return $self->call_path_root.$path.'/';
+}
+
+has call_path_root => (
+	is => 'ro',
+	lazy_build => 1,
+	builder => '_build_call_path_root',
+);
+
+sub _build_call_path_root { '/' }
 
 has caller => (
 	is => 'ro',
@@ -16,5 +37,17 @@ has is_cached => (
 	is => 'ro',
 	default => sub { 1 },
 );
+
+has spice_js_file => (
+	is => 'ro',
+	lazy_build => 1,
+	builder => '_build_spice_js_file',
+);
+
+sub _build_spice_js_file {
+	my ( $self ) = @_;
+	my $filename = $self->caller->share->file('spice.js')->absolute;
+	return -f $filename ? $filename : undef;
+}
 
 1;
