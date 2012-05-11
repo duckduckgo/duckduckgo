1e46ea91b3f18f41523acde4f1e62b8e8a08c816
Torsten Raudssus <torsten@raudss.us>
2012-05-11 03:34:04 +0200

    Stuff

--- lib/DDG/App/Attribution.pm
+++ lib/DDG/App/Attribution.pm
@@ -18,28 +18,25 @@ sub BUILD {
 	my ( $self ) = @_;
 	my $curdir = dir('lib')->absolute;
 	lib->import($curdir->stringify);
-	if (@ARGV) {
-		for (@ARGV) {
-			use_module($_);
-			if ($self->html) {
-				print $_->get_attributions_html;
-				print "\n";
-			} else {
-				my @attributions = @{$_->get_attributions};
-				if (@attributions) {
-					print "\nAttributions for ".$_.":\n\n";
-					while (@attributions) {
-						my $key = shift @attributions;
-						my $value = shift @attributions;
-						print " - ".$key." (".$value.")\n";
-					}
-				} else {
-					print "\nNo attributions for ".$_."\n\n";
+	my @modules = @ARGV ? @ARGV : (); # TODO get complete list of all available modules on no args
+	for (@modules) {
+		use_module($_);
+		if ($self->html) {
+			print $_->get_attributions_html;
+			print "\n";
+		} else {
+			my @attributions = @{$_->get_attributions};
+			if (@attributions) {
+				print "\nAttributions for ".$_.":\n\n";
+				while (@attributions) {
+					my $key = shift @attributions;
+					my $value = shift @attributions;
+					print " - ".$key." (".$value.")\n";
 				}
+			} else {
+				print "\nNo attributions for ".$_."\n\n";
 			}
 		}
-	} else {
-		# TODO, load all and show all attributions
 	}
 	print "\n";
 }
