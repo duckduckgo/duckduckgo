23ca84af1eb6b2abc12e469600c0a4d851712db7
Robert Picard <mail@robert.io>
2012-08-09 21:07:26 -0400

    Add DDG::Meta::Fathead class to specify Fathead specific meta attributes
    The current ones are "mediawiki" (whether or not the abstract comes from
    a mediawiki source) and "title_addon" (the part that will be surrounded
    by parenthesis and appended to the title for every result from that
    Fathead, e.g. "Perl 6" in the title "Str.uc (Perl 6)").

---
+++ lib/DDG/Meta/Fathead.pm
@@ -0,0 +1,74 @@
+package DDG::Meta::Fathead;
+# ABSTRACT: Functions for generating a L<DDG::ZeroClickInfo::Fathead> factory 
+
+use strict;
+use warnings;
+use Package::Stash;
+
+sub fathead_attributes {qw(
+    mediawiki
+    title_addon
+)}
+
+=head1 DESCRIPTION
+
+=cut
+
+=method apply_keywords
+
+Uses a given classname to install the described keywords.
+
+=cut
+
+my %applied;
+
+sub apply_keywords {
+    my ( $class , $target ) = @_;
+
+    return if exists $applied{$target};
+    $applied{$target} = undef;
+
+    my @parts = split( '::' , $target );
+    shift @parts;
+    shift @parts;
+    my $answer_type = lc(join(' ', @parts));
+
+    my $stash = Package::Stash->new($target);
+
+    my %zci_params = (
+        answer_type => $answer_type,
+    );
+
+=keyword fathead
+
+=cut
+
+    $stash->add_symbol('&zci', sub {
+        if (ref $_[0] eq 'HASH') {
+            for (keys %{$_[0]}) {
+                $zci_params{check_fathead_key($_)} = $_[0]->{$_};
+            }
+        } else {
+            while (@_) {
+                my $key = shift;
+                my $value = shift;
+                $zci_params{check_fathead_key($key)} = $value;
+            }
+        }
+    });
+}
+
+=method check_fathead_key
+
+=cut
+
+sub check_fathead_key {
+    my $key = shift;
+    if (grep { $key eq $_ } fathead_attributes) {
+        return $key;
+    } else {
+        croak $key." is not supported on DDG::Meta::Fathead";
+    }
+}
+
+1;
