768f9bddd205327a8463989c70818da6b2dd8ce9
moollaza <moollaza@gmail.com>
2012-11-07 16:26:03 -0500

    added DDG::IsLongtail and DDG::Longtail

---
+++ lib/DDG/IsLongtail.pm
@@ -0,0 +1,19 @@
+package DDG::IsLongtail;
+# ABSTRACT: Role for Longtail
+
+use Moo::Role;
+
+requires qw(
+	handle_request_matches
+);
+
+=head1 DESCRIPTION
+
+This role is for plugins which are Fathead. They need to implement a
+B<handle_request_matches> function.
+
+Please see L<DDG::Meta::RequestHandler> and L<DDG::Meta> for more information.
+
+=cut
+
+1;
---
+++ lib/DDG/Longtail.pm
@@ -0,0 +1,36 @@
+package DDG::Longtail;
+# ABSTRACT: Longtail package for easy keywords
+
+use strict;
+use warnings;
+use DDG::Meta;
+
+sub import {
+	my ( $class, %params ) = @_;
+	my $target = caller;
+
+	DDG::Meta->apply_base_to_package($target);
+	DDG::Meta->apply_longtail_keywords($target);
+}
+
+=head1 SYNOPSIS
+
+  package DDG::Longtail::MyLongtail;
+  # ABSTRACT: My cool Longtail!
+
+  use DDG::Longtail;
+
+  1;
+
+=head1 DESCRIPTION
+
+This is the Longtail Meta class. It injects all the keywords used for
+ZeroClickInfo Longtail. For more information see L<DDG::Meta>.
+
+=head1 SEE ALSO
+
+L<http://duckduckhack.com/>
+
+=cut
+
+1;
