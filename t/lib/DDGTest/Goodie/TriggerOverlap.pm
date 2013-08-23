41c56aef963aefe0ce8c93118a2979f36b6d5d46
moollaza <moollaza@gmail.com>
2013-08-23 15:06:53 -0400

    Updated triggers in TriggerOverlap.pm

--- t/lib/DDGTest/Goodie/TriggerOverlap.pm
+++ t/lib/DDGTest/Goodie/TriggerOverlap.pm
@@ -2,8 +2,8 @@ package DDGTest::Goodie::TriggerOverlap;
 
 use DDG::Goodie;
 
-triggers start => 'myTrigger', 'myTrigger start';
-triggers end => 'myTrigger', 'end myTrigger';
+triggers start => 'myTrigger start';
+triggers end => 'end myTrigger';
 triggers any => 'myTrigger';
 # triggers startend => 'myTrigger', 'myTrigger startend';
 
