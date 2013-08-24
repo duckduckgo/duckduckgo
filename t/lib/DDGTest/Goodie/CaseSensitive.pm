3535db40ff28575b450c66f9f6379714595518b4
moollaza <moollaza@gmail.com>
2013-08-24 14:21:54 -0400

    CaseSensitive.pm - fixed broken handle statement

--- t/lib/DDGTest/Goodie/CaseSensitive.pm
+++ t/lib/DDGTest/Goodie/CaseSensitive.pm
@@ -2,8 +2,8 @@ package DDGTest::Goodie::CaseSensitive;
 
 use DDG::Goodie;
 
-triggers any => 'notCaseSensitive', 'ALLCAPS';
+triggers any => "notCaseSensitive", "ALLCAPS";
 
-handle remainder => sub { $_) };
+handle remainder => sub { $_ };
 
 1;
