c511eba7be27a06dba690701b1687f1def169f5c
russell <russell@duckduckgo.com>
2013-10-03 19:56:29 -0400

    added short location summary string : zip or 'city, country'

--- lib/DDG/Location.pm
+++ lib/DDG/Location.pm
@@ -7,13 +7,30 @@ my @geo_ip_record_attrs = qw( country_code country_code3 country_name region
 	region_name city postal_code latitude longitude time_zone area_code
 	continent_code metro_code );
 
+my @geo_ip_record_s = (@geo_ip_record_attrs, 'loc_str');
+
 sub new_from_geo_ip_record {
 	my ( $class, $geo_ip_record ) = @_;
+
 	if ($geo_ip_record) {
 		my %args;
 		for (@geo_ip_record_attrs) {
 			$args{$_} = $geo_ip_record->$_ if defined $geo_ip_record->$_;
 		}
+
+        # add short location summary string: postal_code if it exists, otherwise 'city, country' or 'region, country'
+        my $loc_str = '';
+        my $city = $geo_ip_record->city || $geo_ip_record->region_name;
+
+        if ($city) {
+            $city .= ', ' . $geo_ip_record->country_name if $geo_ip_record->country_name;
+        } else {
+            $city = '';
+        }
+
+        $args{loc_str} = $geo_ip_record->postal_code || $city;
+
+
 		return $class->new(
 			geo_ip_record => $geo_ip_record,
 			%args,
@@ -26,7 +43,7 @@ sub new_from_geo_ip_record {
 has $_ => (
 	is => 'ro',
 	default => sub { '' }
-) for (@geo_ip_record_attrs);
+) for (@geo_ip_record_s);
 
 has geo_ip_record => (
 	is => 'ro',
