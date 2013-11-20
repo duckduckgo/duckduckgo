4e036ecc6b4c41035db3a62207eca1bbdf81cbd0
Zaahir Moolla <moollaza@fastmail.fm>
2013-11-20 16:55:41 -0500

    Remove handle_request_matches requirement from isSpice and isGoodie role. Now HasRequestHandler role added by RequestHandler.pm when request handler added to class. Now add isSpice and isGoodie in Meta.pm

--- lib/DDG/IsSpice.pm
+++ lib/DDG/IsSpice.pm
@@ -3,16 +3,11 @@ package DDG::IsSpice;
 
 use Moo::Role;
 
-requires qw(
-	handle_request_matches
-);
-
 =head1 DESCRIPTION
 
-This role is for plugins which are Spice. They need to implement a
-B<handle_request_matches> function.
+This role is for plugins which are Spice.
 
-Please see L<DDG::Meta::RequestHandler> and L<DDG::Meta> for more information.
+Please see L<DDG::Meta> for more information.
 
 =cut
 
