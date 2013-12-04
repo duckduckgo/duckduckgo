ea3513d6c024cd4f401892e5fc4d049fd739aa81
Zaahir Moolla <moollaza@fastmail.fm>
2013-12-04 18:31:53 -0500

    Tests - Updated tests to match proper syntax for modifying is_cached from handle func

--- t/lib/DDGTest/Spice/ChangeCached.pm
+++ t/lib/DDGTest/Spice/ChangeCached.pm
@@ -5,14 +5,13 @@ use DDG::Spice;
 triggers any => 'changed caching';
 
 spice to => 'www.mywebsite.com/api/?q=$1';
+
 spice is_cached => 0;
 
 handle remainder => sub {
 	
-	if ($_) {
-		spice is_cached => $_ eq "test" ? 1 : 0;
-		return $_;
-	}
+	return $_, {is_cached => 1} if $_ eq "test";
+	return $_ if $_;
 	return;
 };
 
