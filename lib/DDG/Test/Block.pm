9c13e7da1f3e271c66474936165c2fd9765ecd0e
Eric Johnson <eric.git@iijo.org>
2014-03-01 19:22:32 -0500

    Improve tests for require_example_queries()

--- lib/DDG/Test/Block.pm
+++ lib/DDG/Test/Block.pm
@@ -30,8 +30,8 @@ informations.
 =cut
 
 	$stash->add_symbol('&block_test',sub {
-		my ($result_callback, $plugins, @queries) = @_;
-        my @queries_copy = @queries;
+		my ($result_callback, $plugins, @queries_targets) = @_;
+        my @queries = pairkeys @queries_targets;
 		my @regexp; my @words;
 		for (@$plugins) {
 			load_class($_);
@@ -45,8 +45,8 @@ informations.
 		}
 		my $words_block = @words ? DDG::Block::Words->new( plugins => [@words]) : undef;
 		my $regexp_block = @regexp ? DDG::Block::Regexp->new( plugins => [@regexp]) : undef;
-		while (@queries) {
-			my $query = shift @queries;
+		while (@queries_targets) {
+			my $query = shift @queries_targets;
 			my $request;
 			if (ref $query eq 'DDG::Request') {
 				$request = $query;
@@ -58,7 +58,7 @@ informations.
 					language => test_language('us'),
 				);
 			}
-			my $target = shift @queries;
+			my $target = shift @queries_targets;
 			my $answer = undef;
 			( $answer ) = $words_block->request($request) if $words_block;
 			( $answer ) = $regexp_block->request($request) if $regexp_block && !$answer;
@@ -70,28 +70,38 @@ informations.
 				is($answer,$target,'Checking for not matching on '.$query);
 			}
 		}
-        $class->require_tests_for_example_queries($plugins, \@queries_copy);
+        $class->require_tests_for_example_queries($plugins, \@queries);
 	});
 
 }
 
 sub require_tests_for_example_queries {
-    my ($class, $plugins, $queries) = @_;
+    my ($class, $plugins, $tested_queries) = @_;
 
+    # NOTE: This only requires tests for examples queries in the first plugin
     my $meta = $plugins->[0]->get_meta_information;
     my @example_queries = (
         @{ $meta->{primary_example_queries}   },
         @{ $meta->{secondary_example_queries} },
     );
 
+    # set difference
     my $difference = Set::Scalar->new(@example_queries) - 
-                     Set::Scalar->new(pairkeys @$queries);
+                     Set::Scalar->new(@$tested_queries);
 
     my $note = "\n  Tests for the following queries are missing:\n    " .
-        join("\n    ", $difference->elements) if $difference->elements;
+        join("\n    ", $difference->elements);
 
-    ok( $difference->size == 0, "Tests for example queries exist") ||
-        note $note;
+    # this env variable is only true when running tests in
+    # t/require_example_queries/goodie.t and
+    # t/require_example_queries/spice.t
+    if ($ENV{DDG_REQUIRE_EXAMPLE_QUERIES_TEST}) {
+        die $note unless $difference->size == 0;
+    }
+    else {
+        ok( $difference->size == 0, "Tests for example queries exist") ||
+            note $note;
+    }
 }
 
 1;
--- t/require_example_queries/goodie.t
+++ t/require_example_queries/goodie.t
@@ -2,7 +2,7 @@
 
 use strict;
 use warnings;
-use Test::More;
+use Test::Most;
 
 use FindBin qw($Bin);
 use lib "$Bin/../lib";
@@ -11,20 +11,23 @@ use DDG::Test::Goodie;
 
 zci 'answer_type' => 'goodie_example_queries';
 
-# TODO - need to test for failing tests somehow
-#dies_ok {
-#    ddg_goodie_test(
-#        [qw/ DDGTest::Goodie::RequireExampleQueries /],
-#        'first trigger a' => test_zci('a'),  
-#    );
-#} 'Dies when primary/secondary examples are not tested';
+$ENV{DDG_REQUIRE_EXAMPLE_QUERIES_TEST} = 1;
 
-ddg_goodie_test(
-    [qw/ DDGTest::Goodie::RequireExampleQueries /],
-    'first trigger a'  => test_zci('a'),  
-    'second trigger b' => test_zci('b'),  
-    'second trigger c' => test_zci('c'),  
-    'first trigger d'  => test_zci('d'),  
-);
+dies_ok {
+    ddg_goodie_test(
+        [qw/ DDGTest::Goodie::RequireExampleQueries /],
+        'first trigger a' => test_zci('a'),  
+    );
+} 'Dies when primary/secondary examples are not tested';
+
+lives_ok {
+    ddg_goodie_test(
+        [qw/ DDGTest::Goodie::RequireExampleQueries /],
+        'first trigger a'  => test_zci('a'),  
+        'second trigger b' => test_zci('b'),  
+        'second trigger c' => test_zci('c'),  
+        'first trigger d'  => test_zci('d'),  
+    );
+} 'Lives when primary/secondary examples are tested';
 
 done_testing;
